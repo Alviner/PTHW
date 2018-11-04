2d6b7e34f2e9e4e438a3ab6d64fc17623fb5c8af
BlackFox <sitkineragon@mail.ru>
2018-11-05 09:58:53 +1000

    ex1

--- ex1.py
+++ ex1.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 
-print("Hello world")
+print("Hello world!")
+print("Hello again")
+print("I like typing this.")
+print("Thisd is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not")
+print('I "said" do not touch this.')
