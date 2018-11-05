d5e2b5d3b84e6acb6ca667e2d02547222cda0c48
Alviner <gr.eyed.black.fox@gmail.com>
2018-11-05 12:44:25 +1000

    ex5

--- ex5.py
+++ ex5.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 
-my_name = "Zed A. Show"
-my_age = 35 # not a lie
-my_height = 74 # inches
-my_weight = 180 # ibs
-my_eyes = "Blue"
-my_teeth = 'White'
-my_hair = 'Brown'
+name = "Zed A. Show"
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # ibs
+eyes = "Blue"
+teeth = 'White'
+hair = 'Brown'
 
-print "Let's talk about %s" % my_name
-print "He's %d inches tall." % my_height
-print "He's %d pounds heavy." % my_weight
+print "Let's talk about %s" % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
 print "Actualy that's not too heavy."
-print "He's got %s eyes and %s heir." % (my_eyes, my_hair)
-print "His teeth are usually %s depending on the coffe." % my_teeth
+print "He's got %s eyes and %s heir." % (eyes, hair)
+print "His teeth are usually %s depending on the coffe." % teeth
 
 # this line is tricky, try to get it exactly right
-print "If I add %d, %d, and %d I get %d" % (my_age, my_height, my_weight,
-    my_age + my_height + my_weight)
+print "If I add %d, %d, and %d I get %d" % (age, height, weight,
+    age + height + weight)
